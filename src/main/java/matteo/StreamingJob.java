60d4f30039ea634c08285ee28f68b30a74bf9b9c
matteobucci <matteo.bucci.1995@gmail.com>
2017-09-11 18:51:18 +0200

    Added support to print output to file Refactor of some code

--- src/main/java/matteo/StreamingJob.java
+++ src/main/java/matteo/StreamingJob.java
@@ -10,25 +10,32 @@ import org.apache.commons.cli.*;
 import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+import java.io.File;
 
 public class StreamingJob {
 
-	static  boolean o = false; //Output of debug informations
+
 
 	public static void main(String[] args) throws Exception {
 
 	    int port = 0;
 	    String host = null;
+	    String path = null;
         String[] sciffCmd;
+
+		boolean o = false;
+		boolean printToConsole = false;
 
 		/* Command line arguments */
 
 		Options options = new Options();
 
-		Option outputOption = new Option("o", "output", false, "enable debug output");
-        outputOption.setRequired(false);
-		options.addOption(outputOption);
+		Option debugOption = new Option("o", "output", false, "enable debug output");
+		debugOption.setRequired(false);
+		options.addOption(debugOption);
 
 		Option portOption = new Option("p", "port", true, "port");
         portOption.setRequired(true);
@@ -46,17 +53,35 @@ public class StreamingJob {
         dirOption.setRequired(true);
         options.addOption(dirOption);
 
+		Option outputOption = new Option("c", "console", false, "output to console");
+		outputOption.setRequired(false);
+		options.addOption(outputOption);
+
+		Option fileOption = new Option("f", "file", true, "output to file");
+		fileOption.setRequired(false);
+		options.addOption(fileOption);
+
 		CommandLineParser parser = new DefaultParser();
 		HelpFormatter formatter = new HelpFormatter();
 		CommandLine cmd;
 
 		try {
+
 			cmd = parser.parse(options, args);
 
-            if(cmd.hasOption(outputOption.getOpt())){
+			//Check debug mode
+            if(cmd.hasOption(debugOption.getOpt())){
                 o = true;
-            }
+				System.out.println("Debug mode enabled");
+			}
 
+			//Check output to console
+            if(cmd.hasOption(outputOption.getOpt())){
+				printToConsole = true;
+				System.out.println("Print to console forced");
+			}
+
+			//Check port and host
             port = Integer.parseInt(cmd.getOptionValue(portOption.getOpt()));
             host = cmd.getOptionValue(hostOption.getOpt());
 
@@ -67,6 +92,19 @@ public class StreamingJob {
                 return;
             }
 
+            //Check output to file
+            if(cmd.hasOption(fileOption.getOpt())){
+				path = cmd.getOptionValue(fileOption.getOpt());
+				File file = new File(path);
+				if(file.isDirectory()){
+					System.err.println("File cannot be a directory");
+					System.exit(1);
+					return;
+				}
+				System.out.println("Results are going to be in " + file.getPath());
+			}
+
+			//Check Sciff dirs
             String path_to_swipl = cmd.getOptionValue(swiplOption.getOpt());
             String path_to_AlpBPM = cmd.getOptionValue(dirOption.getOpt());
 
@@ -99,29 +137,38 @@ public class StreamingJob {
 
 		final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
+
         DataStream<String> data = env.socketTextStream(host, port);
 
 		/* Stream flow */
 
-		data.filter(new NotEmptyFilter())
-				.flatMap(new EventSplitter())
-				.map(new EventExtractor())
-				.keyBy(0)
-				.flatMap(new KeyedSciffOperation(sciffCmd, o))
-				.map(new MapFunction<Tuple2<String,String>, Tuple2<String, Integer>>() {
+		SingleOutputStreamOperator<Tuple2<String, Integer>> output = data
+				.filter(new NotEmptyFilter())															//Remove empty lines
+				.flatMap(new EventSplitter())															//Extract single elements
+				.map(new EventExtractor())																//Map key and event
+				.keyBy(0)																		//Group by key
+				.flatMap(new KeyedSciffOperation(sciffCmd, o))											//Run process
+				.map(new MapFunction<Tuple2<String,String>, Tuple2<String, Integer>>() {				//Extract results
 
 					@Override
 					public Tuple2<String, Integer> map(Tuple2<String, String> stringStringTuple2) throws Exception {
 						return new Tuple2<>(stringStringTuple2.f1, 1);
 					}
 				})
-				.keyBy(0)
-				.sum(1)
-				.print();
+				.keyBy(0)																		//Group by results
+				.sum(1);																	//Count results
 
-		// Execute program
+		/* Output logic */
+		if(path != null){
+			output.writeAsText(path); 	//Write to file
+		}
+
+		if(path == null || printToConsole){
+			output.print();				//Write to console if file is not specified or if printToConsole is forced (args c)
+		}
+
+		/* Execute program */
 		env.execute("Sciff launcher");
 	}
-
 
 }
